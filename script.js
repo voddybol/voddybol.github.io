98e2f8eb76418c6e18349c62ca203d5b29a19d4f
voddybol <bol.vadim444@gmail.com>
2022-07-07 00:01:35 +0300

    swiper-slider

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+new Swiper('.image-slider', {
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true, 
+  },
+});
+
+
 $(document).ready(function() {
   $('.section-faq__title').click(function(event) {
     $(this).toggleClass('active').next().slideToggle(300);
