b2f3a09009e01f275760a127ef77adbacf4fd5fb
voddybol <bol.vadim444@gmail.com>
2022-07-07 21:50:12 +0300

    deleted slider

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-new Swiper('.image-slider', {
-  pagination: {
-    el: '.swiper-pagination',
-    type: 'bullets',
-    clickable: true, 
-  },
-});
-
-
 $(document).ready(function() {
   $('.section-faq__title').click(function(event) {
     $(this).toggleClass('active').next().slideToggle(300);
