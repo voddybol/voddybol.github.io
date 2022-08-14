8114797b48e83c33da384e5c21b39d7fc28a6b1c
Vadim Bolotov <107754903+voddybol@users.noreply.github.com>
2022-08-14 21:10:56 +0300

    created script.js

---
+++ js/script.js
@@ -0,0 +1,44 @@
+window.addEventListener('DOMContentLoaded', function() {
+  const swiper = new Swiper('.swiper', {
+    loop: false,
+  
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    scrollbar: {
+      el: '.swiper-scrollbar',
+      hide: true,
+    },
+  });
+
+
+
+$(document).ready(function() {
+  $('.header__burger').click(function(event) {
+    $('.header__burger,.header__menu').toggleClass('active');
+    $('body').toggleClass('lock');
+  });
+});
+
+$(document).ready(function() {
+  $('.faq__item').click(function(event) {
+    if($('.faq__block').hasClass('up')){
+      $('.faq__title').not($(this));
+      $('.faq__text').not($(this).next()).slideUp(300).next().removeClass('active');
+    }
+    $(this).toggleClass('active').next().slideToggle(300);
+  });
+});
+
+$(document).ready(function() {
+  $( "#tabs" ).tabs();
+});
+})
+
